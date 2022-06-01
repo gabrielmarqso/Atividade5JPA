219407594be83ac57f02cdf3e2c528ef469c6f11
Gabriel Marques <59378229+gabrielmarqso@users.noreply.github.com>
2022-05-31 23:09:18 -0300

    Add files via upload

---
+++ src/main/java/entities/Automovel.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Automovel implements Serializable{
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Integer idauto;
+	
+	@Column (name="anoFabricacao", nullable = false)
+	private Integer anoFabricacao;
+	
+	@Column (name = "anoModelo", nullable = false)
+	private Integer anoModelo;
+	
+	@Column (name = "observacoes", nullable = false)
+	private String observacoes;
+	
+	@Column (name = "preco", nullable = false)
+	private Float preco;
+	
+	@Column (name = "kilometragem", nullable = false)
+	private Integer kilometragem;
+
+
+	@OneToOne
+	@JoinColumn(name = "modelo_id")
+	private Modelo modelo;
+
+	public Automovel(Integer idauto, Integer anoFabricacao, Integer anoModelo, String observacoes, Float preco,
+			Integer kilometragem, Modelo modelo) {
+		super();
+		this.idauto = idauto;
+		this.anoFabricacao = anoFabricacao;
+		this.anoModelo = anoModelo;
+		this.observacoes = observacoes;
+		this.preco = preco;
+		this.kilometragem = kilometragem;
+		this.modelo = modelo;
+	}
+}
---
+++ src/main/java/entities/Marca.java
@@ -0,0 +1,35 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Marca implements Serializable{
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Integer idmarca;
+	
+	@Column (name = "idmarca", nullable = false)
+	private String nome;
+	
+	@OneToMany
+	@JoinColumn(name="marca_id")
+	private List<Modelo> modelos;
+
+	public Marca(Integer idmarca, String nome) {
+		super();
+		this.idmarca = idmarca;
+		this.nome = nome;
+	}
+	
+	
+}
